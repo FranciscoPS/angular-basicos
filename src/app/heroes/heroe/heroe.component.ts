0e99409018fa4c17fc4149045c67907577cf5ccd
Francisco  Peralta <franciscoperalta@MacBook-Air-de-Francisco.local>
2022-05-02 14:06:34 -0500

    45. Concepto de one way data binding

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -3,18 +3,27 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-heroe',
-    templateUrl: 'heroe.component.html'   
+    templateUrl: 'heroe.component.html',
+    styleUrls: ['./../../app.component.css']
 })
 export class HeroeComponent{
 
-    public nombre: string = 'Ironman';
-    public edad: number = 45;
+    nombre: string = 'Ironman';
+    edad: number = 45;
 
-    get nombreCapitalizado(){
+    get nombreCapitalizado(): string{
         return this.nombre.toUpperCase();
     }
 
-    public obtenerNombre(): string{
+    obtenerNombre(): string{
         return `${ this.nombre } - ${ this.edad }`;
+    }
+
+    cambiarNombre(): void{
+        this.nombre = 'Spider-man';
+    }
+
+    cambiarEdad(): void{
+        this.edad = 30;
     }
 }
