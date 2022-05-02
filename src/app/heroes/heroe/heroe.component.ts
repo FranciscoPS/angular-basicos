e760ff21abfdd79fb678f4e37d5d47cf279000f9
Francisco  Peralta <franciscoperalta@MacBook-Air-de-Francisco.local>
2022-05-02 13:57:12 -0500

    43. Cambios en el template del componente

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -6,5 +6,15 @@ import { Component } from "@angular/core";
     templateUrl: 'heroe.component.html'   
 })
 export class HeroeComponent{
-    
+
+    public nombre: string = 'Ironman';
+    public edad: number = 45;
+
+    get nombreCapitalizado(){
+        return this.nombre.toUpperCase();
+    }
+
+    public obtenerNombre(): string{
+        return `${ this.nombre } - ${ this.edad }`;
+    }
 }
