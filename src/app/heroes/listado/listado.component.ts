6c974e0fadf44640646b7d824a4157c7f1f5c686
Francisco  Peralta <franciscoperalta@MacBook-Air-de-Francisco.local>
2022-05-03 14:59:27 -0500

    Directiva *ngFor

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -2,13 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listado',
-  templateUrl: './listado.component.html'
+  templateUrl: './listado.component.html',
+  styleUrls: ['./../../app.component.css']
 })
 export class ListadoComponent implements OnInit {
+
+  heroes: string[] = [ 'Spider-man', 'Iron-man', 'Hulk', 'Thor', 'Capitan América' ];
+  heroeBorrado: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  borrarHeroe(): void{
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
 }
